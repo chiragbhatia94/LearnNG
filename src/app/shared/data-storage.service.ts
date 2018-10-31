abf355d8447ff7f3fd31e5a208fb457ed5fe584d
Chirag Bhatia <chiragbhatia94@gmail.com>
2018-10-31 12:29:04 +0530

    Resolved rxjs issues

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import 'rxjs/Rx';
+import { map } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -10,26 +10,28 @@ export class DataStorageService {
   constructor(private http: Http, private recipeService: RecipeService) {}
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-book.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    return this.http.put(
+      'https://portfolio-e9b32.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes()
+    );
   }
 
   getRecipes() {
-    this.http.get('https://ng-recipe-book.firebaseio.com/recipes.json')
-      .map(
-        (response: Response) => {
+    this.http
+      .get('https://portfolio-e9b32.firebaseio.com/recipes.json')
+      .pipe(
+        map((response: Response) => {
           const recipes: Recipe[] = response.json();
-          for (let recipe of recipes) {
+          for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe['ingredients'] = [];
             }
           }
           return recipes;
-        }
+        })
       )
-      .subscribe(
-        (recipes: Recipe[]) => {
-          this.recipeService.setRecipes(recipes);
-        }
-      );
+      .subscribe((recipes: Recipe[]) => {
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }
