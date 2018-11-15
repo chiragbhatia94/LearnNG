aa11ea40bcd34da8ff35c61630fcc270f69e4323
Chirag Bhatia <chiragbhatia94@gmail.com>
2018-11-15 12:13:16 +0530

    Added recipes module

---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from '../auth/auth-guard.service';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesComponent } from './recipes.component';
+
+const routes: Routes = [
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
+      { path: ':id', component: RecipeDetailComponent },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        canActivate: [AuthGuard]
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class RecipesRoutingModule {}
